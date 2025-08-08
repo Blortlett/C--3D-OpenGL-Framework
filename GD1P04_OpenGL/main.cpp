be57e03b177d6571b661c14a07541738b9ed2bb2
Blortlett <45956871+Blortlett@users.noreply.github.com>
2025-08-08 20:30:40 +1200

    Single Quad + Buxfix

--- GD1P04_OpenGL/main.cpp
+++ GD1P04_OpenGL/main.cpp
@@ -13,11 +13,7 @@ GLFWwindow* Window;
 GLuint Program_ColorFade;
 GLuint Program_WorldSpace;
 
-// VAO and VBO for first tri/quad
-GLuint VBO_Tri;
-GLuint VAO_Tri;
-
-// VAO and VBO for second quad
+// VAO and VBO for quad
 GLuint VBO_Quad;
 GLuint VAO_Quad;
 GLuint EBO_Quad;
@@ -25,27 +21,13 @@ GLuint EBO_Quad;
 // Program vars
 float CurrentTime;
 
-// Verticies
-GLfloat Verticies_Quad[] = {
-	// -= First Triangle
-	// Position			// Color
-	 0.0f, -0.8f, 0.0f,	1.0f, 0.0f, 0.0f,
-	-0.5f, 0.0f, 0.0f,	0.0f, 1.0f, 0.0f,
-	 0.5f, 0.0f, 0.0f,	0.0f, 0.0f, 1.0f,
-	 // Second Triangle
-	// Position			// Color
-	 0.0f, 0.8f, 0.0f,	0.75f, 0.0f, 1.0f,
-	-0.5f, 0.0f, 0.0f,	0.0f, 1.0f, 0.0f,
-	 0.5f, 0.0f, 0.0f,	0.0f, 0.0f, 1.0f,
-};
-
 // Vertices for second quad (top-right, using triangle strip)
 GLfloat Vertices_Quad[] = {
 	// Position         // Color
-	 0.0f, 0.4f, 0.0f,  1.0f, 0.0f, 0.0f,
-	 0.0f, 0.8f, 0.0f,  0.0f, 1.0f, 0.0f,
-	 0.5f, 0.4f, 0.0f,  0.0f, 0.0f, 1.0f,
-	 0.5f, 0.8f, 0.0f,  1.0f, 0.0f, 1.0f 
+	 -0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
+	 -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
+	  0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 1.0f,
+	  0.5f,  0.5f, 0.0f,  0.0f, 1.0f, 1.0f
 };
 
 GLuint Indices_Quad[] = {
@@ -55,7 +37,7 @@ GLuint Indices_Quad[] = {
 
 // -= Quad Transform =-
 // Quad Position
-glm::vec3 QuadPosition = glm::vec3(0.5f, 0.5f, 0.5f);
+glm::vec3 QuadPosition = glm::vec3(0.0f, 0.0f, 0.0f);
 glm::mat4 TranslationMat;
 // Quad Rotation
 float QuadRotationAngle = 45.0f;
@@ -94,7 +76,7 @@ void Render()
 
 	// Bind the program and VAO
 	glUseProgram(Program_WorldSpace);
-	glBindVertexArray(VAO_Tri);
+	glBindVertexArray(VAO_Quad);
 
 	// Send variables to the shaders via Uniform
 	GLint CurrentTimeLoc = glGetUniformLocation(Program_WorldSpace, "CurrentTime");
@@ -114,13 +96,9 @@ void Render()
 	//QuadModelMat = TranslationMat * RotationMat * ScaleMat;
 	// TODO ^^
 
-	// Render First Quad
-	// Render the first triangle
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-	// Render the second triangle
-	glDrawArrays(GL_TRIANGLES, 3, 3);
 
-	// Render second quad
+
+	// Render quad
 	glBindVertexArray(VAO_Quad);
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
@@ -173,24 +151,6 @@ int main()
 	Program_WorldSpace = ShaderLoader::CreateProgram(		"Resources/Shaders/WorldSpace.vert",
 															"Resources/Shaders/VertexColorFade.frag");
 
-	// -= Setup first Tri/Quad
-	// 1) Generate the VAO for a triangle
-	glGenVertexArrays(1, &VAO_Tri);
-	glBindVertexArray(VAO_Tri);
-	
-	// 2) Generate the VBO for a Triangle
-	glGenBuffers(1, &VBO_Tri);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO_Tri);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Verticies_Quad), Verticies_Quad, GL_STATIC_DRAW);
-
-	// 3) Set the Vertex Attribute information (how to interpret the vertex data)
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(1);
-
-	// Unbind VAO for first tri
-	glBindVertexArray(0);
 
 	// -= Setup second quad =-
 	// 1) Generate the VAO for the second quad
